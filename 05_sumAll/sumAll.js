968d80e625a6d7f815d65c38142166687cc5f764
ayuboketch <ayuboketch84@gmail.com>
2024-06-14 20:06:01 +0300

    Excersices Complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, lastNum) {
+    // Check for non-number parameters
+    if (typeof firstNum !== 'number' || typeof lastNum !== 'number') {
+        return 'ERROR';
+    }
+    
+    // Check for negative numbers
+    if (firstNum < 0 || lastNum < 0) {
+        return 'ERROR';
+    }
+    
+    // Determine the start and end of the range
+    let start = firstNum < lastNum ? firstNum : lastNum;
+    let end = firstNum < lastNum ? lastNum : firstNum;
+    
+    let sum = 0;
+    // Iterate from start to end (inclusive) and sum the numbers
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5 / 9;
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * 9 / 5) + 32;
+  return Math.round(fahrenheit * 10) / 10;
 };
+
+// Ex
 
 // Do not edit below this line
 module.exports = {
